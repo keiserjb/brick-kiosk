1a493030cd3b5dd14a276516d39fd1f0623dbbe3
keiserjb <keiserjb@yahoo.com>
2023-02-08 13:04:42 -0500

    react helment and favicon

--- src/components/App.js
+++ src/components/App.js
@@ -1,11 +1,18 @@
 import 'bootstrap/dist/css/bootstrap.min.css';
 import '../css/react_bricks.css';
 import BrickList from './BrickList';
+import {Helmet} from "react-helmet";
+
 
 function App() {
   return (
     <div className="App">
-
+      <Helmet>
+                <meta charSet="utf-8" />
+                <title>AMA Walk of Fame</title>
+        <link rel="canonical" href="https://eloquent-kitten-8839bc.netlify.app/" />
+        <meta name="description" content="AMA Walk of Fame Brick directory" />
+            </Helmet>
       <main className="App__main">
         <BrickList />
       </main>
