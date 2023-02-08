4768055eb8807dc38bef849ced9f4619840f1a0c
keiserjb <keiserjb@yahoo.com>
2023-02-08 12:50:00 -0500

    initial app commit

---
+++ src/components/App.js
@@ -0,0 +1,16 @@
+import 'bootstrap/dist/css/bootstrap.min.css';
+import '../css/react_bricks.css';
+import BrickList from './BrickList';
+
+function App() {
+  return (
+    <div className="App">
+
+      <main className="App__main">
+        <BrickList />
+      </main>
+    </div>
+  );
+}
+
+export default App;
--- src/index.js
+++ src/index.js
@@ -1,17 +1,21 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import React from 'react'
+import ReactDOM from 'react-dom'
+import './css/react_bricks.css'
+import App from './components/App'
+import BrickProvider from './context/BrickProvider'
+import JsonApiProvider from './context/JsonApiProvider'
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
-);
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+
+
+
+ReactDOM.render(
+  <JsonApiProvider>
+    <BrickProvider>
+      <App />
+    </BrickProvider>
+  </JsonApiProvider>,
+  document.getElementById('root')
+)
+
+
