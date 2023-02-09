afcfa225b3f9567f06baffa4dbdb65a59f393b9d
keiserjb <keiserjb@yahoo.com>
2023-02-09 13:28:40 -0500

    add brick details

--- src/components/Brick.jsx
+++ src/components/Brick.jsx
@@ -1,5 +1,6 @@
 import React from 'react';
 import PropTypes from 'prop-types';
+import BrickDetail from './BrickDetail';
 
 
 const Brick = ({
@@ -22,7 +23,7 @@ const Brick = ({
                }) => (
   // <div className="col-md-4 mx-auto">
     <div>
-    <a className="brick-link" style={{display: background100 ? "block" : "none"}} href={`https://test-137a472c-153f916d-14c5c44b.app.devpanel.com/${path}`}>
+    <div className="brick-link" style={{display: background100 ? "block" : "none"}}>
       <div className="brick-wrapper-view-100" style={{display: background100 ? "flex" : "none"}}>
 
         <div className="brick-contents-view">
@@ -30,11 +31,13 @@ const Brick = ({
           <p className="brick-text-view-100 text-center">{secondLine100}</p>
           <p className="brick-text-view-100 text-center">{thirdLine100}</p>
         </div>
-
       </div>
-    </a>
+      <div className="brick-button text-center">
+        <BrickDetail title={title} background100={background100} firstLine100={firstLine100} secondLine100={secondLine100} thirdLine100={thirdLine100} />
+        </div>
+    </div>
 
-    <a className="brick-link" style={{display: background500 ? "block" : "none"}} href={`https://test-137a472c-153f916d-14c5c44b.app.devpanel.com/${path}`}>
+    <div className="brick-link" style={{display: background500 ? "block" : "none"}}>
       <div className="brick-wrapper-view-500" style={{display: background500 ? "flex" : "none"}}>
         <div className="brick-contents-view">
           <p className="brick-text-view-500 text-center">{firstLine500}</p>
@@ -42,9 +45,12 @@ const Brick = ({
           <p className="brick-text-view-500 text-center">{thirdLine500}</p>
         </div>
       </div>
-    </a>
+      <div className="brick-button text-center">
+        <BrickDetail title={title} background500={background500} firstLine500={firstLine500} secondLine500={secondLine500} thirdLine500={thirdLine500} />
+        </div>
+    </div>
 
-    <a className="brick-link" style={{display: background1000 ? "block" : "none"}} href={`https://test-137a472c-153f916d-14c5c44b.app.devpanel.com/${path}`}>
+    <div className="brick-link" style={{display: background1000 ? "block" : "none"}}>
       <div className="brick-wrapper-view-1000" style={{display: background1000 ? "flex" : "none"}}>
 
         <div className="brick-contents-view-1000">
@@ -55,12 +61,14 @@ const Brick = ({
           <p className="brick-text-view-1000 text-center">{fifthLine1000}</p>
         </div>
       </div>
-    </a>
+      <div className="brick-button text-center">
+        <BrickDetail title={title} background1000={background1000} firstLine1000={firstLine1000} secondLine1000={secondLine1000} thirdLine1000={thirdLine1000} fourthLine1000={fourthLine1000} fifthLine1000={fifthLine1000} />
+    </div>
     <div className="container justify-content-center align-items-end" style={{display: title ? "none" : "flex"}}>
       <p>There were no results.  Please try again.</p>
     </div>
   </div>
-
+</div>
 )
 
 Brick.propTypes = {
---
+++ src/components/BrickDetail.jsx
@@ -0,0 +1,76 @@
+import React, { useState } from 'react';
+import Button from 'react-bootstrap/Button';
+import Modal from 'react-bootstrap/Modal';
+
+function BrickDetail({ title,
+                 firstLine100,
+                 secondLine100,
+                 thirdLine100,
+                 firstLine500,
+                 secondLine500,
+                 thirdLine500,
+                 firstLine1000,
+                 secondLine1000,
+                 thirdLine1000,
+                 fourthLine1000,
+                 fifthLine1000,
+                 background100,
+                 background500,
+                 background1000}) {
+  const [show, setShow] = useState(false);
+
+  const handleClose = () => setShow(false);
+  const handleShow = () => setShow(true);
+  return (
+    <>
+      <Button variant="primary" onClick={handleShow}>
+        View Brick
+      </Button>
+
+      <Modal
+        show={show}
+        onHide={handleClose}
+        keyboard={false}
+      >
+        <Modal.Header closeButton>
+          <Modal.Title><h2>{title}</h2></Modal.Title>
+        </Modal.Header>
+        <Modal.Body>
+          <div className="brick-wrapper-100" style={{display: background100 ? "flex" : "none"}}>
+
+            <div className="brick-contents">
+              <p className="brick-text-100 text-center">{firstLine100}</p>
+              <p className="brick-text-100 text-center">{secondLine100}</p>
+              <p className="brick-text-100 text-center">{thirdLine100}</p>
+            </div>
+          </div>
+          <div className="brick-wrapper-500" style={{display: background500 ? "flex" : "none"}}>
+
+            <div className="brick-contents">
+              <p className="brick-text-500 text-center">{firstLine500}</p>
+              <p className="brick-text-500 text-center">{secondLine500}</p>
+              <p className="brick-text-500 text-center">{thirdLine500}</p>
+            </div>
+          </div>
+          <div className="brick-wrapper-1000" style={{display: background1000 ? "flex" : "none"}}>
+
+            <div className="brick-contents-1000">
+              <p className="brick-text-1000 text-center">{firstLine1000}</p>
+              <p className="brick-text-1000 text-center">{secondLine1000}</p>
+              <p className="brick-text-1000 text-center">{thirdLine1000}</p>
+              <p className="brick-text-1000 text-center">{fourthLine1000}</p>
+              <p className="brick-text-1000 text-center">{fifthLine1000}</p>
+            </div>
+        </div>
+        </Modal.Body>
+        <Modal.Footer>
+          <Button variant="secondary" onClick={handleClose}>
+            Close
+          </Button>
+        </Modal.Footer>
+      </Modal>
+    </>
+  );
+}
+
+export default BrickDetail
